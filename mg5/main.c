cb1564f96b4c7bd5de53b45bd4acc14ad12fb735
tobysimpson <tobygeneva@hotmail.com>
2025-07-26 10:00:02 +0200

    Initial Commit

---
+++ mg5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  mg5
+//
+//  Created by Toby Simpson on 26/07/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
